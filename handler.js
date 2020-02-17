6f3e64917402d5a1d366de83f8d2930c3ce9c22d
Nichola Riley <nicholajaneriley@gmail.com>
2020-02-17 20:15:53 +0000

    backend uploaded to shared git

--- handler.js
+++ handler.js
@@ -1,18 +1,156 @@
-'use strict';
+const express = require("express");
+const cors = require("cors");
+const serverlessHttp = require("serverless-http");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "improv"
+});
+
+// GET /tasks
+
+function getRandom(data){
+
+    let randomPostion = Math.floor(Math.random() * data.length);
+    let randomSuggestion = data[randomPostion];
+    response.status(200).json(
+         {
+             suggestion : randomSuggestion         
+         });
+
+}
+
+app.get("/suggestion", function (request, response) {
+
+    connection.query("SELECT * FROM Suggestions", function (err, data) {
+
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+
+       //     getRandom(data);
+           let randomPostion = Math.floor(Math.random() * data.length);
+           let randomSuggestion = data[randomPostion];
+           response.status(200).json(
+                {
+                    suggestion : randomSuggestion 
+                    //suggestion: data[randomPosition] - this got an internal server error        
+                });
+        };
+    });
+
+});
+
+app.get("/suggestion/:id", function (request, response) {
+
+    const id = request.params.id;
+
+    connection.query("Select * from Suggestions where typeId = ?", [id], function (err, data) {
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            let randomPostion = Math.floor(Math.random() * data.length);
+            let randomSuggestion = data[randomPostion];
+            response.status(200).json(
+                 {
+                     suggestion : randomSuggestion         
+                 });
+        };
+    });
+});
+
+
+app.get("/types", function (request, response) {
+
+    connection.query("SELECT * FROM Types", function (err, data) {
+
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.status(200).json(
+                {
+                    types: data
+                });
+        };
+    });
+});
+
+
+// POST 
+
+app.post("/suggestion", function (request, response) {
+
+    const newSuggestion = request.body;
+    connection.query("INSERT INTO Suggestions SET ?", [newSuggestion], function (err, data) {
+
+        if (err) {
+            response.status(500).json({
+                error:
+                    err
+            })
+        }
+        else {
+            newSuggestion.id = data.insertId;
+            response.status(201).json(newSuggestion);
+        }
+    });
+
+});
+
+// PUT 
+app.put("/suggestion/:id", function (request, response) {
+
+    const id = request.params.id;
+
+    connection.query("UPDATE Suggestions set favourite = true WHERE id=?", [id], function (err, data) {
+
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.status(200).json({
+                suggestion: data
+            });
+        };
+    });
+
+});
+
+// DELETE
+
+app.delete("/suggestion/:id", function (request, response) {
+
+    const id = request.params.id;
+
+    connection.query("Delete FROM Suggestions WHERE id = ?", [id], function (err, data) {
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.sendStatus(200);
+        };
+    });
+});
+
+module.exports.app = serverlessHttp(app);
