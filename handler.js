4aaa5ca4691f9ede4ad6304b35bbeea117aa1980
Carly Tarett <carlytarett@hotmail.com>
2020-02-26 15:59:47 +0000

    Code refactored

--- handler.js
+++ handler.js
@@ -6,8 +6,6 @@ const mysql = require("mysql");
 
 const app = express();
 
-
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -18,17 +16,10 @@ const connection = mysql.createConnection({
     database: "improv"
 });
 
-// GET /tasks
-
 function getRandom(data, response) {
 
     let randomPostion = Math.floor(Math.random() * data.length);
     return data[randomPostion];
-    // response.status(200).json(
-    //     {
-    //         suggestion: randomSuggestion
-    //     });
-
 }
 
 app.get("/suggestion", function (request, response) {
@@ -55,8 +46,6 @@ app.get("/suggestion", function (request, response) {
 
 app.get("/suggestion/:id", function (request, response) {
 
-
-   // let answer = {};
     let id = request.params.id;
     let secondId = 0;
     let thirdId = 0;
@@ -69,15 +58,12 @@ app.get("/suggestion/:id", function (request, response) {
         secondId = 7;
     }
 
-    if (id == 6 )
-    {
+    if (id == 6) {
         insult = true;
         id = 8;
         secondId = 9;
         thirdId = 10;
     }
-
-    console.log(id);
 
     connection.query("Select * from Suggestions where typeId = ?", [id], function (err, data) {
         if (err) {
@@ -86,8 +72,6 @@ app.get("/suggestion/:id", function (request, response) {
             })
         }
         else {
-
-//console.log(data);
             let randomSuggestion = getRandom(data, response);
 
             if (book) {
@@ -99,31 +83,25 @@ app.get("/suggestion/:id", function (request, response) {
                     }
                     else {
 
-                                let nextBit = getRandom(data, response);
+                        let nextBit = getRandom(data, response);
 
-                              //  console.log(nextBit);
+                        let answer = 'The '.concat(nextBit.suggestion, ' ', randomSuggestion.suggestion);
 
-                                let answer = 'The '.concat(nextBit.suggestion, ' ', randomSuggestion.suggestion  );
-                           //     console.log(answer);
+                        randomSuggestion = { id: null, suggestion: answer, typeId: id, favourite: null }
+                        console.log("to start with in here its ", randomSuggestion);
+                        response.status(200).json(
+                            {
+                                suggestion: randomSuggestion
+                            });
 
-                                randomSuggestion = { id: null, suggestion: answer, typeId: id, favourite: null}
-                       console.log("to start with in here its ", randomSuggestion);
-                       response.status(200).json(
-                        {
-                            suggestion: randomSuggestion
-                        });
-                       
-                                // closing inside else
                     }
-               //closing inside conn quiery     }
-                })
-            
-            
 
-            // closing if book
+                })
+
+
             }
-            else if( insult){
-                // getting a 9 - noun
+            else if (insult) {
+
                 connection.query("Select * from Suggestions where typeId = ?", [secondId], function (err, data) {
                     if (err) {
                         response.status(500).json({
@@ -135,9 +113,6 @@ app.get("/suggestion/:id", function (request, response) {
                         let nounObject = getRandom(data, response);
                         let noun = nounObject.suggestion;
 
-                        console.log("noun is : ", noun)
-
-
                         connection.query("Select * from Suggestions where typeId = ?", [thirdId], function (err, data) {
                             if (err) {
                                 response.status(500).json({
@@ -146,91 +121,43 @@ app.get("/suggestion/:id", function (request, response) {
                             }
                             else {
 
-
-console.log("I still know ", noun)
                                 let verbObject = getRandom(data, response);
                                 let verb = verbObject.suggestion;
                                 let randomSuggestionAdjective = randomSuggestion.suggestion;
 
-                                let answer = 'You\'re so ' .concat(randomSuggestionAdjective, ' you make ', noun, ' ', verb);
+                                let answer = 'You\'re so '.concat(randomSuggestionAdjective, ' you make ', noun, ' ', verb);
 
                                 response.status(200).json(
                                     {
-                                        suggestion: { id: null, suggestion: answer, typeId: id, favourite: null}
+                                        suggestion: { id: null, suggestion: answer, typeId: id, favourite: null }
                                     });
-
-                    // console.log(answer);
-                    //         let answer = 'This is an insult too';
-
-                    //          randomSuggestion = { id: null, suggestion: answer, typeId: id, favourite: null}
-
-                    //          response.status(200).json(
-                    //         {
-                    //             suggestion: randomSuggestion
-
-                    //          });
-        
-                                //         let nextBit = getRandom(data, response);
-        
-                                //       //  console.log(nextBit);
-        
-                                //         let answer = 'The '.concat(nextBit.suggestion, ' ', randomSuggestion.suggestion  );
-                                //    //     console.log(answer);
-        
-                                //         randomSuggestion = { id: null, suggestion: answer, typeId: id, favourite: null}
-                                //         console.log("to start with in here its ", randomSuggestion);
-                                //         response.status(200).json(
-                                //             {
-                                //                 suggestion: randomSuggestion
-                                //             });
-                               
-                            // closing inside else
                             }
-                       //closing insult second inside conn quiery     }
+
                         })
-                    
-                    
-                       
-                    // closing inside else
+
                     }
-               //closing inssult first inside conn quiery     }
+
                 })
-            
-            
-
-
-
-
-
-            // closing if insult
-            }
-            else{
-
-            // if a normal one.
-
-            console.log("but ehre it's back to" , randomSuggestion);
-
-            response.status(200).json(
-                {
-                    suggestion: randomSuggestion
-                });
 
             }
+            else {
 
-        //closing outside else
+                response.status(200).json(
+                    {
+                        suggestion: randomSuggestion
+                    });
+
+            }
         }
-
-    //closing first connection query 
     });
 
-    //closing app.get
 });
 
 
 app.get("/types", function (request, response) {
 
-  //  connection.query("SELECT * FROM Types where display = true", function (err, data) {
-        connection.query("SELECT * FROM Types", function (err, data) {
+    //  connection.query("SELECT * FROM Types where display = true", function (err, data) {
+    connection.query("SELECT * FROM Types", function (err, data) {
 
         if (err) {
             response.status(500).json({
@@ -248,25 +175,21 @@ app.get("/types", function (request, response) {
 
 app.get("/creatableTypes", function (request, response) {
 
-        connection.query("SELECT * FROM Types where display = true", function (err, data) {
-         // connection.query("SELECT * FROM Types", function (err, data) {
-  
-          if (err) {
-              response.status(500).json({
-                  error: err
-              })
-          }
-          else {
-              response.status(200).json(
-                  {
-                      types: data
-                  });
-          };
-      });
-  });
+    connection.query("SELECT * FROM Types where display = true", function (err, data) {
 
-
-// POST 
+        if (err) {
+            response.status(500).json({
+                error: err
+            })
+        }
+        else {
+            response.status(200).json(
+                {
+                    types: data
+                });
+        };
+    });
+});
 
 app.post("/suggestion", function (request, response) {
 
@@ -287,7 +210,6 @@ app.post("/suggestion", function (request, response) {
 
 });
 
-// PUT 
 app.put("/suggestion/:id", function (request, response) {
 
     const id = request.params.id;
@@ -307,8 +229,6 @@ app.put("/suggestion/:id", function (request, response) {
     });
 
 });
-
-// DELETE
 
 app.delete("/suggestion/:id", function (request, response) {
 
